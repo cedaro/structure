c1acb1484c978b929617882d5a32d02e952b7bc0
Brady Vercher <brady@blazersix.com>
2025-03-23 06:24:40 -0700

    Update example using version 1.0.0 of cedaro/wp-plugin.

--- bootstrap.php
+++ bootstrap.php
@@ -7,15 +7,17 @@
  * @license   GPL-2.0+
  */
 
+namespace Structure;
+
+use Cedaro\WP\Plugin\PluginFactory;
+use Cedaro\WP\Plugin\Provider\I18n;
+use Structure\Container;
+use Structure\ServiceProvider;
+
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-
-use Cedaro\WP\Plugin\Container;
-use Cedaro\WP\Plugin\PluginFactory;
-use Cedaro\WP\Plugin\Provider\I18n;
-use Structure\ServiceProvider;
 
 /**
  * Load the Composer autoloader.
@@ -25,14 +27,14 @@ if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 }
 
 // Create the main plugin instance.
-$structure = PluginFactory::create(
-	'structure',
-	__DIR__ . '/structure.php'
-);
+$structure = PluginFactory::create( 'structure', __FILE__ );
+
+$container = new Container();
+$container->register( new ServiceProvider() );
 
 // Register a service provider.
 $structure
-	->get_container()
+	->set_container( $container )
 	->register( new ServiceProvider() );
 
 // Register hook providers.
---
+++ php/Container.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Container class.
+ *
+ * @package   Structure
+ * @copyright Copyright (c) 2025 Cedaro, LLC
+ * @license   GPL-2.0+
+ */
+
+namespace Structure;
+
+use Pimple\Container as PimpleContainer;
+use Psr\Container\ContainerInterface;
+
+/**
+ * Container.
+ *
+ * @package Structure
+ */
+class Container extends PimpleContainer implements ContainerInterface {
+	/**
+	 * Finds an entry of the container by its identifier and returns it.
+	 *
+	 * @param string $id Identifier of the entry to look for.
+	 * @return mixed Entry.
+	 */
+	public function get( $id ) {
+		return $this->offsetGet( $id );
+	}
+
+	/**
+	 * Whether the container has an entry for the given identifier.
+	 *
+	 * @param string $id Identifier of the entry to look for.
+	 * @return bool
+	 */
+	public function has( $id ): bool {
+		return $this->offsetExists( $id );
+	}
+}
